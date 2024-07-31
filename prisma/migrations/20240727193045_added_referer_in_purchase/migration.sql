beb63fbf14954cf60da6d4822fd0cd97e5ea96f9
mrdiv <mr.divyanshu10@gmail.com>
2024-07-31 16:59:12 +0530

    uff

--- prisma/migrations/20240727193045_added_referer_in_purchase/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `referrer` on the `Purchase` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Purchase" DROP COLUMN "referrer",
-ADD COLUMN     "referrerId" INTEGER;
